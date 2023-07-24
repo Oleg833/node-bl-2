5b98d78262ba3b6fb6e69be9a69785dfbf29aa07
Oleg833 <oleg.kovtun447@gmail.com>
2023-07-25 02:57:34 +0300

    add3

---
+++ function.js
@@ -0,0 +1,6 @@
+const sum = (num1, num2) => num1 + num2;
+const minus = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) => num1 / num2;
+
+module.exports = { sum, minus, mult, div };
---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+const argv = require("yargs").argv;
+const { sum, minus, mult, div } = require("./function");
+
+// TODO: рефакторити
+function invokeAction({ action, num1, num2 }) {
+  switch (action) {
+    case "sum":
+      console.log("sum =", sum(num1, num2));
+      break;
+
+    case "minus":
+      console.log("result =", minus(num1, num2));
+      break;
+
+    case "mult":
+      console.log("result =", mult(num1, num2));
+      break;
+
+    case "div":
+      console.log("result =", div(num1, num2));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
